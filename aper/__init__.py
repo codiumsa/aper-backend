3b4c3b7f1e12ef91d33918bc56cebb0e4dc08b1d
vanecanhete <vanecanhete@gmail.com>
2019-06-15 16:59:20 -0400

    CORS: hacer pip install flask_cors

--- aper/__init__.py
+++ aper/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
 from flask_login import LoginManager
+from flask_cors import CORS 
 
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object('config')
 app.config.from_pyfile('config.py', silent=True)
+
+CORS(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
