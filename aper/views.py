ba685115f9aa2f1e604b101eabd1aff0ff984f0b
Diego Aguilera <daguilera110@gmail.com>
2019-08-27 11:37:16 -0400

    Status code based on permissions

--- aper/views.py
+++ aper/views.py
@@ -19,7 +19,7 @@ def get_current_user():
 @login_required
 def users():
     if current_user.role != 'ADMIN':
-        return 'UNAUTHORIZED', 401
+        return 'FORBIDDEN', 403
     else:
         users = [u.serialize() for u in User.query.all()]
         return jsonify(users)
@@ -29,7 +29,7 @@ def users():
 @login_required
 def open_gate():
     if (current_user.role != 'ADMIN' and current_user.role != 'USER'):
-        return 'UNAUTHORIZED', 401
+        return 'FORBIDDEN', 403
     else:
         allowed_users = User.allowed_users()
         if current_user in allowed_users:
@@ -49,7 +49,7 @@ def open_gate():
 @login_required
 def not_using():
     if (current_user.role != 'ADMIN' and current_user.role != 'USER'):
-        return 'UNAUTHORIZED', 401
+        return 'FORBIDDEN', 403
     else:
         if current_user.absent_on != datetime.date.today():
             current_user.absent_on = date.today()
@@ -65,7 +65,7 @@ def not_using():
 @login_required
 def change_order():
     if current_user.role != 'ADMIN':
-        return 'UNAUTHORIZED', 401
+        return 'FORBIDDEN', 403
     else:
         arr = request.form.get('ids').split(',')
         for id, index in enumerate(arr, start = 1):
@@ -78,7 +78,7 @@ def change_order():
 @login_required
 def change_roles():
     if current_user.role != 'ADMIN':
-        return 'UNAUTHORIZED', 401
+        return 'FORBIDDEN', 403
     else:
         arr = request.form.get('ids').split(',')
         for id, index in enumerate(arr, start = 1):
@@ -90,13 +90,19 @@ def change_roles():
 @app.route('/absent', methods=['GET'])
 @login_required
 def isabsent():
-    if current_user.absent_on == datetime.date.today():
-        return 'yes'
+    if (current_user.role != 'ADMIN' and current_user.role != 'USER'):
+        return 'FORBIDDEN', 403
     else:
-        return 'no'
+        if current_user.absent_on == datetime.date.today():
+            return 'yes'
+        else:
+            return 'no'
 
 @app.route('/last_use', methods=['GET'])
 @login_required
 def getlastuse():
-    user = User.query.order_by(User.last_use.desc()).first()
-    return user.serialize()
+    if (current_user.role != 'ADMIN' and current_user.role != 'USER'):
+        return 'FORBIDDEN', 403
+    else:
+        user = User.query.order_by(User.last_use.desc()).first()
+        return user.serialize()
